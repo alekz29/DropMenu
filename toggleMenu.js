3c3fef12e801d2e9dacc03a493987eea2eeb7b87
Aleksander Zych <Konfucjussz29@gmail.com>
2017-10-11 14:18:04 +0200

    added animation button_arrow

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,13 +1,22 @@
-const toggleMenu = () => {
-    const menu = document.getElementById('menu');
+const menu = document.getElementById('menu')
+const arrow = document.getElementById('arrow')
 
+const toggleMenu = () => {
     if (menu.className === 'expanded') {
-        menu.classList.remove('expanded');
+        menu.classList.remove('expanded')
         menu.classList.add('collapsed')
+        arrow.classList.remove('openArrow')
+        arrow.classList.add('closeArrow')
+
     }
     else {
-        menu.classList.remove('collapsed');
+        menu.classList.remove('collapsed')
         menu.classList.add('expanded')
+        arrow.classList.remove('closeArrow')
+        arrow.classList.add('openArrow')
     }
-    console.log(menu.classList)
-};
+}
+
+menu.addEventListener('click', toggleMenu);
+
+
